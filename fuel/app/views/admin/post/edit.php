8982eecbdf775a5365597ce2a7162522e0754512
Eric Ouyang <contact@ericouyang.com>
2013-03-29 13:23:47 -0400

    a couple more slash fixes

--- fuel/app/views/admin/post/edit.php
+++ fuel/app/views/admin/post/edit.php
@@ -1,7 +1,7 @@
 <h2>Editing Post</h2>
 <br>
 
-<?php echo render('admin\post/_form'); ?>
+<?php echo render('admin/post/_form'); ?>
 <p>
 	<?php echo Html::anchor('admin/post/view/'.$post->id, 'View'); ?> |
 	<?php echo Html::anchor('admin/post', 'Back'); ?></p>
