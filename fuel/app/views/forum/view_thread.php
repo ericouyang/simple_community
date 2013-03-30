1de0d6d55441bad48950f8d1afee8ed1cdf92c3b
Eric Ouyang <contact@ericouyang.com>
2013-03-29 23:49:22 -0400

    create post link fix

--- fuel/app/views/forum/view_thread.php
+++ fuel/app/views/forum/view_thread.php
@@ -1,9 +1,9 @@
 <div class="page-header">
-  <?php echo Html::anchor('forum/create_post/'.$thread->id, 'Post a reply', array('class'=>'btn btn-primary pull-right')); ?>
+  <?php echo Html::anchor('forum/'.$thread->forum_id.'/thread/'.$thread->id.'/create_post', 'Post a reply', array('class'=>'btn btn-primary pull-right')); ?>
   <h2><?php echo $thread->title; ?></h2>
 </div>
 <?php if(count($thread->posts) == 0): ?>
-  <h5>No posts in this thread. <?php echo Html::anchor('forum/create_post/'.$thread->id, 'Create one!'); ?></h5>
+  <h5>No posts in this thread. <?php echo Html::anchor('forum/'.$thread->forum_id.'/thread/'.$thread->id.'/create_post', 'Create one!'); ?></h5>
 <?php else: ?>
 <table class="table table-striped table-bordered">
   <tbody>
